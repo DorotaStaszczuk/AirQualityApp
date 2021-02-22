d18160f07eb7a8f437badc371fa4971702382cde
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-02-22 10:13:08 +0100

    feat: display address in a different form

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const formSubmit = document.querySelector('.form-submit');
-    let displayAddress1 = document.querySelector('.display-address1');
-    let displayAddress2 = document.querySelector('.display-address2');
+    let displayAddress = document.querySelector('.display-address');
     const pm1 = document.querySelector('.pm1');
     const pm10 = document.querySelector('.pm10');
     const pm25 = document.querySelector('.pm25');
@@ -26,8 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
             .then((data) => data.json())
             .then((response2) => {
                 console.log(response2);
-                displayAddress1.innerText = `${response2[0].address.displayAddress1}`;
-                displayAddress2.innerText = `ul. ${response2[0].address.displayAddress2}`;
+                displayAddress.innerText = `${response2[0].address.displayAddress1}, ul. ${response2[0].address.displayAddress2}`;
             });
 
         fetch(`https://airapi.airly.eu/v2/measurements/nearest?lat=${lat}&lng=${lang}&maxDistanceKM=5`, {
@@ -44,9 +42,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 pm1.innerText = `${response2.current.values[0].name}: ${response2.current.values[0].value} µg/m3`;
                 pm25.innerText = `PM2,5: ${response2.current.values[1].value} µg/m3`;
                 pm10.innerText = `${response2.current.values[2].name}: ${response2.current.values[2].value} µg/m3`;
-                pressure.innerText = `Ciśnienie: ${response2.current.values[3].value} hPa`;
-                humidity.innerText = `Wilgotność: ${response2.current.values[4].value} %`;
-                temperature.innerText = `Temperatura: ${response2.current.values[5].value} °C`;
+                pressure.innerText = ` ${response2.current.values[3].value} hPa`;
+                humidity.innerText = ` ${response2.current.values[4].value} %`;
+                temperature.innerText = ` ${response2.current.values[5].value} °C`;
 
                 advice.innerText = `${response2.current.indexes[0].description} ${response2.current.indexes[0].advice}`;
                 if (response2.current.indexes[0].level === 'LOW' || response2.current.indexes[0].level === 'VERY_LOW') {
