d2a1749c23aa2583870313e0af1bbe84e135ab30
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-03-03 19:25:31 +0100

    feat: add error message, change structure

--- main.js
+++ main.js
@@ -9,25 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const temperature = document.querySelector('.temperature');
     let advice = document.querySelector('.advice');
     const responseSection = document.querySelector('.response-section');
+    const errorMessage = document.querySelector('.error');
 
     formSubmit.addEventListener('click', function(e) {
         e.preventDefault();
-        responseSection.classList.contains('none') && responseSection.classList.remove('none');
         let lat = document.querySelector('.szerokosc').value;
         let lang = document.querySelector('.dlugosc').value;
-
-        fetch(`https://airapi.airly.eu/v2/installations/nearest?lat=${lat}&lng=${lang}&maxDistanceKM=5`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                apikey: config['apiKey'],
-            },
-        })
-            .then((data) => data.json())
-            .then((response2) => {
-                console.log(response2);
-                displayAddress.innerText = `${response2[0].address.displayAddress1}, ul. ${response2[0].address.displayAddress2}`;
-            });
+        responseSection.classList.add('none');
 
         fetch(`https://airapi.airly.eu/v2/measurements/nearest?lat=${lat}&lng=${lang}&maxDistanceKM=5`, {
             method: 'GET',
@@ -40,23 +28,45 @@ document.addEventListener('DOMContentLoaded', function () {
             .then((data) => data.json())
             .then((response2) => {
                 console.log(response2);
-                pm1.innerText = `${response2.current.values[0].name}: ${response2.current.values[0].value} µg/m3`;
-                pm25.innerText = `PM2,5: ${response2.current.values[1].value} µg/m3`;
-                pm10.innerText = `${response2.current.values[2].name}: ${response2.current.values[2].value} µg/m3`;
-                pressure.innerText = ` ${response2.current.values[3].value} hPa`;
-                humidity.innerText = ` ${response2.current.values[4].value} %`;
-                temperature.innerText = ` ${response2.current.values[5].value} °C`;
-
-                advice.innerText = `${response2.current.indexes[0].description} ${response2.current.indexes[0].advice}`;
-                if (response2.current.indexes[0].level === 'LOW' || response2.current.indexes[0].level === 'VERY_LOW') {
-                    responseSection.classList.add('green');
-                } else if (
-                    response2.current.indexes[0].level === 'HIGH' ||
-                    response2.current.indexes[0].level === 'VERY_HIGH'
-                ) {
-                    responseSection.classList.add('red');
+                if (response2.errorCode === 'INSTALLATION_NOT_FOUND'){
+                    errorMessage.innerText = 'W odległości 5 km od podanego punktu nie znaleziono żadnej instalacji, spróbuj ponownie'
+                    errorMessage.classList.contains('none') && errorMessage.classList.remove('none');
                 }
-                else { responseSection.classList.add('yellow')}
+                else {
+                    errorMessage.classList.add('none');
+                    responseSection.classList.contains('none') && responseSection.classList.remove('none');
+                    pm1.innerText = `${response2.current.values[0].value}`;
+                    pm25.innerText = `${response2.current.values[1].value}`;
+                    pm10.innerText = `${response2.current.values[2].value}`;
+                    pressure.innerText = ` ${response2.current.values[3].value} hPa`;
+                    humidity.innerText = ` ${response2.current.values[4].value} %`;
+                    temperature.innerText = ` ${response2.current.values[5].value} °C`;
+
+                    advice.innerText = `${response2.current.indexes[0].description} ${response2.current.indexes[0].advice}`;
+                    if (response2.current.indexes[0].level === 'LOW' || response2.current.indexes[0].level === 'VERY_LOW') {
+                        responseSection.classList.add('green');
+                    } else if (
+                        response2.current.indexes[0].level === 'HIGH' ||
+                        response2.current.indexes[0].level === 'VERY_HIGH'
+                    ) {
+                        responseSection.classList.add('red');
+                    }
+                    else { responseSection.classList.add('yellow')}
+
+                    fetch(`https://airapi.airly.eu/v2/installations/nearest?lat=${lat}&lng=${lang}&maxDistanceKM=5`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            apikey: config['apiKey'],
+                        },
+                    })
+                    .then((data) => data.json())
+                    .then((response2) => {
+                        console.log(response2);
+                        responseSection.classList.contains('none') && responseSection.classList.remove('none');
+                        displayAddress.innerText = `${response2[0].address.displayAddress1}, ul. ${response2[0].address.displayAddress2}`;
+                    });
+                }
             });
     });
 });
